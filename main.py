3cbb2c98153a583b3ffd57c13dcb44da34fab874
Cindy Ho <57569111+cho102@users.noreply.github.com>
2026-09-11 15:52:33 -0700

    Modify Excel export to include images and new headers
    Updated the Excel sheet to include product pictures and additional details.

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import os
 from openpyxl import Workbook
+from openpyxl.drawing.image import Image
 
+#access photos folder
 photo_folder = "photos"
 
 #images 
@@ -15,7 +17,10 @@ product_names = [file.split(".")[0] for file in image_files]
 print("Image files in the folder:")
 for file in image_files:
     print(file)
-    print("Product name:", file.split(".")[0])
+    # print("Product name:", file.split(".")[0])
+print("Image files in the folder:")
+for product in product_names:
+    print(product)
 
 #create a new Excel workbook
 workbook = Workbook()
@@ -24,7 +29,38 @@ workbook = Workbook()
 sheet = workbook.active
 
 #name the worksheet
-sheet.title = "Product Names"
+sheet.title = "inStockInventory"
 
 #create column headers
-sheet['A1'] = "Product Name"
+sheet['A1'] = "Product Picture"
+sheet['A2'] = "Product Name"
+sheet['A3'] = "Colors"
+sheet['A4'] = "Qty"
+sheet['A5'] = "Price"
+
+row = 2
+
+for file in image_files:
+    product_name = file.split(".")[0]
+
+    # Create the image
+    image_path = os.path.join(photo_folder, file)
+    image = Image(image_path)
+
+    # Resize the image
+    image.width = 150
+    image.height = 150
+
+    # Put the image into column A
+    worksheet.add_image(image, f"A{row}")
+
+    # Put the product name into column B
+    sheet.cell(row=row, column=2, value=product_name)
+
+row += 1
+
+#save excel
+workbook.save("inStockInventory.xlsx")
+
+#check
+print("Excel file created successfully!")
