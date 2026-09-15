9f731afa35af1e07e0561f079b6a7b3ce3ad0f16
Cindy Ho <57569111+cho102@users.noreply.github.com>
2026-09-15 14:29:53 -0700

    clean up comments and polishing

--- main.py
+++ main.py
@@ -14,26 +14,11 @@ files = os.listdir(photo_folder)
 image_files = [file for file in files 
                if file.lower().endswith(('.png', '.jpg', '.jpeg', '.gif'))]
 
-#find product names from image files
-#product_names = [file.split(".")[0] for file in image_files]
-
-#check
-# print("Image files in the folder:")
-# for file in image_files:
-#     print(file)
-#     # print("Product name:", file.split(".")[0])
-# print("Image files in the folder:")
-# for product in product_names:
-#     print(product)
-
 #create a new Excel workbook
 workbook = Workbook()
 
 #select the active worksheet
 sheet = workbook.active
-
-#name the worksheet
-sheet.title = "inStockInventory"
 
 #create column headers
 sheet['A1'] = "Product Picture"
@@ -77,8 +62,6 @@ with sync_playwright() as p:
      
     page.goto("http://192.168.1.12/som/query_sm.aspx")
 
-
-
     #read image files and add to excel
     files = os.listdir(photo_folder)
     curr_row = 2
@@ -104,7 +87,6 @@ with sync_playwright() as p:
         sku = file.split(".")[0]
         # print(f"Adding product: {sku} at row {curr_row}")
 
-
         #GET COST & INVENTORY
         cost, inventory = get_inventory(page, sku)
         # print(f"\nFinal inventory list for SKU {sku}:", inventory)
@@ -118,7 +100,6 @@ with sync_playwright() as p:
             sheet.cell(row=inventory_row, column=price_col, value=cost)
             inventory_row += 1
 
-        
         # Create the image
         image_path = os.path.join(photo_folder, file)
         image = Image(image_path)
@@ -152,11 +133,10 @@ with sync_playwright() as p:
         print("rows used:", rows_used)
         print("current row:", curr_row)
 
-        
-
     #save excel
     date = datetime.now().strftime("%Y%m%d")
-    workbook.save(f"inStockInventory_{date}.xlsx")
+    name = "inStockInventory"
+    workbook.save(f"{name}_{date}.xlsx")
     browser.close()
 
 #check
